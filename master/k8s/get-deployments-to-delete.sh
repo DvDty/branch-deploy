bf67b79caeb59e977fbd834ec0993c7adbb284ef
Pavel Grancharov <dvdtygc@gmail.com>
2024-01-27 22:04:00 +0200

    Move script that fetches "deployments to be deleted" to a file.

---
+++ master/k8s/get-deployments-to-delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kubectl get deployments -o=jsonpath='{range .items[*]}{.metadata.name} {.metadata.annotations.lifespan} {"\n"}{end}' | while read deployment lifespan; do
+    if [ "$lifespan" != "" ]; then
+        current_minus_lifespan=$(date -d "$lifespan minute ago" +%s)
+        creation_timestamp=$(kubectl get deployment "$deployment" -o=jsonpath='{.metadata.creationTimestamp}')
+        creation_seconds=$(date -d "$creation_timestamp" +%s)
+
+        if [ "$current_minus_lifespan" -ge "$creation_seconds" ]; then
+            kubectl delete deployment "$deployment"
+        fi
+    fi
+done
