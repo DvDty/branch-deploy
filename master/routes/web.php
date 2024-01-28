a089d0fe2c16b65af08c272da93c5c5130f8f808
Pavel Grancharov <dvdtygc@gmail.com>
2024-01-28 22:27:30 +0200

    Refactor to use k8s API.

--- master/routes/web.php
+++ master/routes/web.php
@@ -5,20 +5,7 @@ use App\Kubectl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function (Request $request, Kubectl $kubectl, Github $github) {
-    dd($kubectl->getDeployments());
-
     if ($request->has('branch') && $request->has('time')) {
         $kubectl->createInstance($request->input('branch'), (int) $request->input('time'));
     }
