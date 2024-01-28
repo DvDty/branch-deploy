2bf5eef45f5a61766c86f37c7a6c28a7e0e37f76
Pavel Grancharov <dvdtygc@gmail.com>
2024-01-28 02:14:29 +0200

    Try to run workflow from inside.

--- application/app/Models/User.php
+++ application/app/Models/User.php
@@ -11,8 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens;
-    use HasFactory;
-    use Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
